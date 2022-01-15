84f4d4d3d747c6ca21df52d1ac0d9356abea2bfd
Shiv Bhonde <shivbhonde04@gmail.com>
2022-01-15 17:01:21 +0530

    add config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Footer, Navbar, Services, Transactions, Welcome } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="min-h-screen">
+			<div className="gradient-bg-welcome">
+				<Navbar />
+				<Welcome />
+			</div>
+			<Services />
+			<Transactions />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
