d7f640cf5f146f0318260753d16dc38006a41351
Shiv Bhonde <shivbhonde04@gmail.com>
2022-01-24 15:14:32 +0530

    add bg to no metamask page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { Footer, Navbar, Transactions, Welcome } from './components';
 const App = () => {
 	if (!window.ethereum) {
 		return (
-			<div className="w-full h-screen flex justify-center items-center bg-black">
-				<h1 className="text-2xl text-white">
-					Please intall wallet like Metamask
+			<div className="w-full h-screen flex justify-center items-center gradient-bg-welcome">
+				<h1 className="text-2xl text-white text-center">
+					Metamask or other EIP-1102 / EIP-1193 compliant wallet not found,
+					<br />
+					Please install Metamask
 				</h1>
 			</div>
 		);
